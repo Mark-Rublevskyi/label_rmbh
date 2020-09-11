6d294f8695f1b08186b5178377763ead3b3b602f
Mark Rublevskiyy <57556412+Mark-Rublevskiy@users.noreply.github.com>
2020-09-11 14:00:56 +0300

    last section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+ $('.slick').slick({
+    dots: false,
+    slidesToShow: 2,
+    slidesToScroll: 2,   
+    Infinite: true,
+    centerMode: true,
+    prevArrow: "<button type='button' class='custom-arrow slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='custom-arrow slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+})
